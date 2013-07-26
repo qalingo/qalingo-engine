99900973ba9be4708306b67b4a4cf7645be235ac
denis <denis@Hoteia-Laptop>
2013-07-26 16:23:48 +0200

    clean view bean

--- qalingo-apis/qalingo-api-core/qalingo-api-core-common-web-fo/src/main/java/fr/hoteia/qalingo/web/mvc/viewbean/CustomerViewBean.java
+++ qalingo-apis/qalingo-api-core/qalingo-api-core-common-web-fo/src/main/java/fr/hoteia/qalingo/web/mvc/viewbean/CustomerViewBean.java
@@ -20,70 +20,60 @@ public class CustomerViewBean extends AbstractViewBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 6264101125517957897L;
 	
-	private String firstnameValue;
-	private String lastnameValue;
-	private String emailValue;
-	private String dateCreateValue;
-	private String dateUpdateValue;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String dateCreate;
+	private String dateUpdate;
 
 	private Map<String, ValueBean> customerAttributes = new HashMap<String, ValueBean>();
-	
-	private String editUrl;
 
-	public String getFirstnameValue() {
-		return firstnameValue;
-	}
+	public String getFirstname() {
+    	return firstname;
+    }
 
-	public void setFirstnameValue(String firstnameValue) {
-		this.firstnameValue = firstnameValue;
-	}
+	public void setFirstname(String firstname) {
+    	this.firstname = firstname;
+    }
 
-	public String getLastnameValue() {
-		return lastnameValue;
-	}
+	public String getLastname() {
+    	return lastname;
+    }
 
-	public void setLastnameValue(String lastnameValue) {
-		this.lastnameValue = lastnameValue;
-	}
+	public void setLastname(String lastname) {
+    	this.lastname = lastname;
+    }
 
-	public String getEmailValue() {
-		return emailValue;
-	}
+	public String getEmail() {
+    	return email;
+    }
 
-	public void setEmailValue(String emailValue) {
-		this.emailValue = emailValue;
-	}
+	public void setEmail(String email) {
+    	this.email = email;
+    }
 
-	public String getDateCreateValue() {
-		return dateCreateValue;
-	}
+	public String getDateCreate() {
+    	return dateCreate;
+    }
 
-	public void setDateCreateValue(String dateCreateValue) {
-		this.dateCreateValue = dateCreateValue;
-	}
+	public void setDateCreate(String dateCreate) {
+    	this.dateCreate = dateCreate;
+    }
 
-	public String getDateUpdateValue() {
-		return dateUpdateValue;
-	}
+	public String getDateUpdate() {
+    	return dateUpdate;
+    }
 
-	public void setDateUpdateValue(String dateUpdateValue) {
-		this.dateUpdateValue = dateUpdateValue;
-	}
-	
+	public void setDateUpdate(String dateUpdate) {
+    	this.dateUpdate = dateUpdate;
+    }
+
 	public Map<String, ValueBean> getCustomerAttributes() {
-		return customerAttributes;
-	}
-	
+    	return customerAttributes;
+    }
+
 	public void setCustomerAttributes(Map<String, ValueBean> customerAttributes) {
-		this.customerAttributes = customerAttributes;
-	}
-	
-	public String getEditUrl() {
-		return editUrl;
-	}
-	
-	public void setEditUrl(String editUrl) {
-		this.editUrl = editUrl;
-	}
+    	this.customerAttributes = customerAttributes;
+    }
 	
 }
