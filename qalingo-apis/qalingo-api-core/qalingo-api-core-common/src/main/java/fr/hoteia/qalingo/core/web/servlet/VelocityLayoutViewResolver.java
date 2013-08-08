6261fe626e69fcb81f7f399dce03261fd07d6173
denis <denis@Hoteia-Laptop>
2013-08-08 16:08:39 +0200

    setExposeModelAttributes

--- qalingo-apis/qalingo-api-core/qalingo-api-core-common/src/main/java/fr/hoteia/qalingo/core/web/servlet/VelocityLayoutViewResolver.java
+++ qalingo-apis/qalingo-api-core/qalingo-api-core-common/src/main/java/fr/hoteia/qalingo/core/web/servlet/VelocityLayoutViewResolver.java
@@ -9,11 +9,24 @@
  */
 package fr.hoteia.qalingo.core.web.servlet;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 
 public class VelocityLayoutViewResolver extends org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver {
-	
+
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
-	
+
+	@Override
+	protected View createView(String viewName, Locale locale) throws Exception {
+		View view = super.createView(viewName, locale);
+		if (view instanceof RedirectView) {
+			((RedirectView) view).setExposeModelAttributes(false);
+		}
+		return view;
+	}
+
 }
