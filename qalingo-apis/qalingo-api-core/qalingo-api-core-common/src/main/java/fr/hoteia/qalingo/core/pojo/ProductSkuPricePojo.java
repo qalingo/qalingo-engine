419cc4520965151a76bf2312ef93a5c4fd0c20b0
FingolfinTEK <fingolfintek@gmail.com>
2013-10-15 11:04:21 +0200

    ProductSkuPrice mapper and pojo

---
+++ qalingo-apis/qalingo-api-core/qalingo-api-core-common/src/main/java/fr/hoteia/qalingo/core/pojo/ProductSkuPricePojo.java
@@ -0,0 +1,64 @@
+package fr.hoteia.qalingo.core.pojo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+import fr.hoteia.qalingo.core.domain.CurrencyReferential;
+
+public class ProductSkuPricePojo {
+
+    private Long id;
+    private int version;
+    private BigDecimal price;
+    private CurrencyReferentialPojo currency;
+    private Long marketAreaId;
+    private Long retailerId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public CurrencyReferentialPojo getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(CurrencyReferentialPojo currency) {
+        this.currency = currency;
+    }
+
+    public Long getMarketAreaId() {
+        return marketAreaId;
+    }
+
+    public void setMarketAreaId(Long marketAreaId) {
+        this.marketAreaId = marketAreaId;
+    }
+
+    public Long getRetailerId() {
+        return retailerId;
+    }
+
+    public void setRetailerId(Long retailerId) {
+        this.retailerId = retailerId;
+    }
+}
