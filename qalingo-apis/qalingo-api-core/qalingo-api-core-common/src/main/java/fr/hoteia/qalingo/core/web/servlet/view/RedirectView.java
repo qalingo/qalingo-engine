2c53108b198c8a310afb1ea8e1cdd9dbab765fe2
denis <denis@Hoteia-Laptop>
2013-11-14 22:36:05 +0100

    refactoring package name + folder name

--- qalingo-apis/qalingo-api-core/qalingo-api-core-common/src/main/java/fr/hoteia/qalingo/core/web/servlet/view/RedirectView.java
+++
@@ -1,9 +0,0 @@
-package fr.hoteia.qalingo.core.web.servlet.view;
-
-public class RedirectView extends org.springframework.web.servlet.view.RedirectView {
-
-	public RedirectView(String url) {
-		super(url);
-		setExposeModelAttributes(false);
-    }
-}
--- qalingo-apis/qalingo-api-core/qalingo-api-core-common/src/main/java/fr/hoteia/qalingo/core/web/validation/annotation/TwoFieldsLink.java
+++
@@ -1,6 +0,0 @@
-package fr.hoteia.qalingo.core.web.validation.annotation;
-
-public interface TwoFieldsLink<T> {
-	T getField();
-	T getField2();
-}
