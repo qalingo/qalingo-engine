a2d74b15312ef96b3e24f2e349115e8cb29f24ae
baotran109 <baotran@xoan.in>
2013-12-30 15:25:06 +0700

    comit for merge

--- apis/api-web/api-web-fo-mcommerce/src/main/java/org/hoteia/qalingo/web/mvc/controller/common/StoreLocationController.java
+++ apis/api-web/api-web-fo-mcommerce/src/main/java/org/hoteia/qalingo/web/mvc/controller/common/StoreLocationController.java
@@ -18,13 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import org.hoteia.qalingo.core.domain.Store;
 import org.hoteia.qalingo.core.domain.enumtype.FoUrls;
 import org.hoteia.qalingo.core.service.RetailerService;
 import org.hoteia.qalingo.core.web.mvc.viewbean.StoreLocatorViewBean;
 import org.hoteia.qalingo.core.web.servlet.ModelAndViewThemeDevice;
 import org.hoteia.qalingo.web.mvc.controller.AbstractMCommerceController;
+
 
 /**
  * 
@@ -41,8 +41,7 @@ public class StoreLocationController extends AbstractMCommerceController {
 		
 		final List<Store> stores = storeService.findStores();
 		final StoreLocatorViewBean storeLocator = frontofficeViewBeanFactory.buildStoreLocatorViewBean(requestUtil.getRequestData(request), stores);
-		modelAndView.addObject("storeLocator", storeLocator);
-		
+		modelAndView.addObject("storeLocator", storeLocator);		
         return modelAndView;
 	}
  
