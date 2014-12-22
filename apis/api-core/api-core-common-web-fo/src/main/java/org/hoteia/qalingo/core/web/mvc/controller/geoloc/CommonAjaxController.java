18044cde245ee5412080a5a797765ffddf7101c0
denis <denis@Hoteia-Laptop>
2014-12-22 20:42:49 +0100

    fix

---
+++ apis/api-core/api-core-common-web-fo/src/main/java/org/hoteia/qalingo/core/web/mvc/controller/geoloc/CommonAjaxController.java
@@ -0,0 +1,67 @@
+package org.hoteia.qalingo.core.web.mvc.controller.geoloc;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hoteia.qalingo.core.domain.enumtype.FoUrls;
+import org.hoteia.qalingo.core.pojo.RequestData;
+import org.hoteia.qalingo.core.pojo.cart.FoMessagePojo;
+import org.hoteia.qalingo.core.pojo.FoNavigatorGeolocationPojo;
+import org.hoteia.qalingo.core.service.GeolocService;
+import org.hoteia.qalingo.web.mvc.controller.AbstractMCommerceController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 
+ */
+@Controller("commonAjaxController")
+public class CommonAjaxController extends AbstractMCommerceController {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	protected GeolocService geolocService;
+	
+    @RequestMapping(value = FoUrls.NAVIGATOR_GEOLOCATION_AJAX_URL, method = RequestMethod.POST)
+    @ResponseBody
+    public FoNavigatorGeolocationPojo navigatorGeolocation(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final RequestData requestData = requestUtil.getRequestData(request);
+        final Locale locale = requestData.getLocale();
+        final String latitude = request.getParameter("latitude");
+        final String longitude = request.getParameter("longitude");
+
+        final FoNavigatorGeolocationPojo navigatorGeolocation = new FoNavigatorGeolocationPojo();
+        navigatorGeolocation.setLatitude(latitude);
+        navigatorGeolocation.setLongitude(longitude);
+        
+        try {
+            requestUtil.handleGeolocLatitudeLongitude(requestData, latitude, longitude);
+            
+            FoMessagePojo successMessage = new FoMessagePojo();
+            successMessage.setId("success-navigator-geolocation");
+            Object[] messageParams = { latitude, longitude };
+            successMessage.setMessage(getSpecificMessage("GEOLOCATION", "navigator_geolocation_success_message", messageParams, locale));
+            navigatorGeolocation.getSuccessMessages().add(successMessage);
+            return navigatorGeolocation;
+            
+        } catch (Exception e) {
+            logger.error("", e);
+            FoMessagePojo errorMessage = new FoMessagePojo();
+            errorMessage.setId("error-navigator-geolocation");
+            Object[] messageParams = { latitude, longitude };
+            errorMessage.setMessage(getSpecificMessage("GEOLOCATION", "navigator_geolocation_error_message", messageParams, locale));
+            navigatorGeolocation.getErrorMessages().add(errorMessage);
+            navigatorGeolocation.setStatuts(false);
+            return navigatorGeolocation;
+        }
+    }
+    
+}
