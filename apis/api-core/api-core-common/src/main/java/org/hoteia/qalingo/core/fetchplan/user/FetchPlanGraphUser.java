b645c2cf13aa57ac820f12ca2e0b87afc89b0143
denis <denis@Hoteia-Laptop>
2015-03-05 21:46:03 +0100

    fix

--- apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/fetchplan/user/FetchPlanGraphUser.java
+++ apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/fetchplan/user/FetchPlanGraphUser.java
@@ -34,8 +34,8 @@ public class FetchPlanGraphUser {
         fetchplans.add(new SpecificFetchMode(User_.company.getName()));
         fetchplans.add(new SpecificFetchMode(User_.groups.getName()));
         fetchplans.add(new SpecificFetchMode(User_.credentials.getName()));
-        fetchplans.add(new SpecificFetchMode(UserGroup_.roles.getName(), new SpecificAlias(UserGroup_.groups + "." + UserGroup_.roles.getName())));
-        fetchplans.add(new SpecificFetchMode(UserRole_.permissions.getName(), new SpecificAlias(UserGroup_.groups + "." + UserGroup_.roles + "." + UserRole_.permissions)));
+        fetchplans.add(new SpecificFetchMode(UserGroup_.roles.getName(), new SpecificAlias(User_.groups + "." + UserGroup_.roles.getName())));
+        fetchplans.add(new SpecificFetchMode(UserRole_.permissions.getName(), new SpecificAlias(User_.groups + "." + UserGroup_.roles + "." + UserRole_.permissions)));
         fetchplans.add(new SpecificFetchMode(User_.connectionLogs.getName()));
         return new FetchPlan(fetchplans);
     }
