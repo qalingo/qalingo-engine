27d7c489287b8f07eb3be40ee3ce743a17eaccf8
denis <denis@Hoteia-Laptop>
2015-04-10 15:26:27 +0200

    menu description

--- apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/web/mvc/viewbean/MenuViewBean.java
+++ apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/web/mvc/viewbean/MenuViewBean.java
@@ -24,6 +24,7 @@ public class MenuViewBean extends AbstractViewBean {
 
     protected String key;
     protected String name;
+    protected String description;
     protected String alt;
     protected String img;
     protected String url;
@@ -54,6 +55,14 @@ public class MenuViewBean extends AbstractViewBean {
 
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getAlt() {
