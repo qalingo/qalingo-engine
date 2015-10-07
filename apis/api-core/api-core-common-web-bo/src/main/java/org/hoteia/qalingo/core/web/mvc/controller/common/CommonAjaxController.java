730d3d8e8a922630df1fab1cd8b909f85906bb06
Denis <Denis@Hoteia-M4800>
2015-10-07 21:59:37 +0200

    add CommonAjaxController

---
+++ apis/api-core/api-core-common-web-bo/src/main/java/org/hoteia/qalingo/core/web/mvc/controller/common/CommonAjaxController.java
@@ -0,0 +1,18 @@
+package org.hoteia.qalingo.core.web.mvc.controller.common;
+
+import org.hoteia.qalingo.core.web.mvc.controller.AbstractBackofficeQalingoController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 
+ */
+@Controller("commonAjaxController")
+public class CommonAjaxController extends AbstractBackofficeQalingoController {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	
+    
+}
