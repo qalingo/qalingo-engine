5c21af476d270f84ca494a25ea78d9673589aade
denis <denis@hoteia>
2015-10-23 13:35:17 +0200

    fix

--- apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/web/mvc/factory/ViewBeanFactory.java
+++ apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/web/mvc/factory/ViewBeanFactory.java
@@ -2040,7 +2040,9 @@ public class ViewBeanFactory extends AbstractViewBeanFactory {
      */
     public AssetViewBean buildViewBeanAsset(final RequestData requestData, final Asset asset) throws Exception {
         final AssetViewBean assetViewBean = new AssetViewBean();
-        assetViewBean.setId(asset.getId().toString());
+        if(asset.getId() != null){
+            assetViewBean.setId(asset.getId().toString());
+        }
         assetViewBean.setName(asset.getName());
         assetViewBean.setDescription(asset.getDescription());
         if(StringUtils.isNotEmpty(asset.getSeo())){
