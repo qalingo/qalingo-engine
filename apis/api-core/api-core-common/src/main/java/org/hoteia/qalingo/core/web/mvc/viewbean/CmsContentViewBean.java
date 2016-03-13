831e372a2c6dfee995b14f29f83269e609c8def4
Denis <Denis@Hoteia-M4800>
2016-03-13 20:11:22 +0100

    fix

--- apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/web/mvc/viewbean/CmsContentViewBean.java
+++ apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/web/mvc/viewbean/CmsContentViewBean.java
@@ -1,8 +1,13 @@
 package org.hoteia.qalingo.core.web.mvc.viewbean;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.hibernate.Hibernate;
 
 public class CmsContentViewBean extends AbstractViewBean {
 
@@ -153,7 +158,13 @@ public class CmsContentViewBean extends AbstractViewBean {
 	}
 
 	public List<CmsContentBlockViewBean> getSortedBlocks() {
-		return blocks;
+	    List<CmsContentBlockViewBean> sortedCmsContentBlocks = null;
+        if (blocks != null 
+                && Hibernate.isInitialized(blocks)) {
+            sortedCmsContentBlocks = new LinkedList<CmsContentBlockViewBean>(blocks);
+            Collections.sort(sortedCmsContentBlocks, new CmsContentBlockViewBeanComparator());
+        }
+        return sortedCmsContentBlocks;
 	}
 	
 	public CmsContentBlockViewBean getDefaultBlock() {
@@ -209,6 +220,16 @@ public class CmsContentViewBean extends AbstractViewBean {
         return assetsByType;
     }
     
+    public List<AssetViewBean> getSortedAssets() {
+        List<AssetViewBean> sortedAssets = null;
+        if (assets != null 
+                && Hibernate.isInitialized(assets)) {
+            sortedAssets = new LinkedList<AssetViewBean>(assets);
+            Collections.sort(sortedAssets, new AssetViewBeanComparator());
+        }
+        return sortedAssets;
+    }
+    
     public String getAssetPath(String type) {
         for (Iterator<AssetViewBean> iterator = assets.iterator(); iterator.hasNext();) {
             AssetViewBean assetViewBean = (AssetViewBean) iterator.next();
@@ -252,7 +273,7 @@ public class CmsContentViewBean extends AbstractViewBean {
     }
 
     public AssetViewBean getDefaultAsset() {
-        for (Iterator<AssetViewBean> iterator = assets.iterator(); iterator.hasNext();) {
+        for (Iterator<AssetViewBean> iterator = getSortedAssets().iterator(); iterator.hasNext();) {
             AssetViewBean assetViewBean = (AssetViewBean) iterator.next();
             if("default".equalsIgnoreCase(assetViewBean.getType())){
                 return assetViewBean;
@@ -262,7 +283,7 @@ public class CmsContentViewBean extends AbstractViewBean {
     }
     
     public String getDefaultImage() {
-        for (Iterator<AssetViewBean> iterator = assets.iterator(); iterator.hasNext();) {
+        for (Iterator<AssetViewBean> iterator = getSortedAssets().iterator(); iterator.hasNext();) {
             AssetViewBean assetViewBean = (AssetViewBean) iterator.next();
             if("default".equalsIgnoreCase(assetViewBean.getType())){
                 return assetViewBean.getAbsoluteWebPath();
@@ -307,4 +328,30 @@ public class CmsContentViewBean extends AbstractViewBean {
 		this.dateCreateMonthYear = dateCreateMonthYear;
 	}
 
+	public class CmsContentBlockViewBeanComparator implements Comparator<CmsContentBlockViewBean> {
+	    public int compare(CmsContentBlockViewBean o1, CmsContentBlockViewBean o2) {
+	        if (o1 != null && o2 != null) {
+	            int result = ((Integer)o1.getOrdering()).compareTo(((Integer)o2.getOrdering()));
+	            if(result == 0){
+	                return (new Long(o1.getId())).compareTo((new Long(o2.getId())));
+	            }
+	            return result;
+	        }
+	        return 0;
+	    }
+	}
+	
+    public class AssetViewBeanComparator implements Comparator<AssetViewBean> {
+        public int compare(AssetViewBean o1, AssetViewBean o2) {
+            if (o1 != null && o2 != null) {
+                int result = ((Integer)o1.getOrdering()).compareTo(((Integer)o2.getOrdering()));
+                if(result == 0){
+                    return (new Long(o1.getId())).compareTo((new Long(o2.getId())));
+                }
+                return result;
+            }
+            return 0;
+        }
+    }
+	
 }
