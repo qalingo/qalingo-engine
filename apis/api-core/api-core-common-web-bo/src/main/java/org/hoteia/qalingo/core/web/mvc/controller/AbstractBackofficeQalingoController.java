7c61182c1abb5f2a62f6a04ecb2b5936e32d068f
Denis <Denis@Hoteia-M4800>
2016-03-29 20:38:46 +0200

    fix

--- apis/api-core/api-core-common-web-bo/src/main/java/org/hoteia/qalingo/core/web/mvc/controller/AbstractBackofficeQalingoController.java
+++ apis/api-core/api-core-common-web-bo/src/main/java/org/hoteia/qalingo/core/web/mvc/controller/AbstractBackofficeQalingoController.java
@@ -9,12 +9,18 @@
  */
 package org.hoteia.qalingo.core.web.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.hoteia.qalingo.core.ModelConstants;
+import org.hoteia.qalingo.core.domain.Localization;
+import org.hoteia.qalingo.core.domain.MarketArea;
+import org.hoteia.qalingo.core.domain.MarketPlace;
+import org.hoteia.qalingo.core.domain.enumtype.BoUrls;
 import org.hoteia.qalingo.core.i18n.FoMessageKey;
 import org.hoteia.qalingo.core.i18n.enumtype.I18nKeyValueUniverse;
 import org.hoteia.qalingo.core.i18n.enumtype.ScopeCommonMessage;
@@ -30,6 +36,7 @@ import org.hoteia.qalingo.core.service.UserService;
 import org.hoteia.qalingo.core.service.WebBackofficeService;
 import org.hoteia.qalingo.core.web.mvc.factory.BackofficeFormFactory;
 import org.hoteia.qalingo.core.web.mvc.factory.BackofficeViewBeanFactory;
+import org.hoteia.qalingo.core.web.mvc.viewbean.HeadLinkViewBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -160,6 +167,45 @@ public abstract class AbstractBackofficeQalingoController extends AbstractQaling
     
     protected String getSpecificMessage(String scope, String key, Object[] params, Locale locale){
         return coreMessageSource.getSpecificMessage(I18nKeyValueUniverse.BO.getPropertyKey(), scope, key, params, locale);
+    }
+    
+    protected List<HeadLinkViewBean> buildConditionsOfUseLinkViewBeans(final RequestData requestData, BoUrls url) {
+        final MarketPlace marketPlace = requestData.getMarketPlace();
+        final MarketArea marketArea = requestData.getMarketArea();
+        
+        List<Long> allOpenedMarketAreaIds = marketService.findMarketAreaOpenedByMarketPlace(marketPlace);
+        List<MarketArea> allMarketArea = new ArrayList<MarketArea>();
+        for (Long marketareaId : allOpenedMarketAreaIds) {
+            MarketArea reloadedMarketArea = marketService.getMarketAreaById(marketareaId);
+            allMarketArea.add(reloadedMarketArea);
+        }
+        List<HeadLinkViewBean> alternateLinks = new ArrayList<HeadLinkViewBean>();
+        try {
+            for (MarketArea marketAreaIt : allMarketArea) {
+                for (Localization localizationIt : marketAreaIt.getLocalizations()) {
+                    if(!marketAreaIt.getCode().equals(marketArea.getCode())){
+    
+                        RequestData requestDataStore = new RequestData();
+                        requestDataStore.setMarketPlace(marketPlace);
+                        requestDataStore.setMarket(marketAreaIt.getMarket());
+                        requestDataStore.setMarketArea(marketAreaIt);
+                        requestDataStore.setMarketAreaLocalization(localizationIt);
+                        requestDataStore.setContextNameValue("FO_MCOMMERCE");
+                        
+                        String alternateUrl = backofficeUrlService.buildAbsoluteUrl(requestDataStore, backofficeUrlService.generateUrl(url, requestDataStore));
+                        String hreflang = localizationIt.getCode().replace("_", "-");
+                        if(marketAreaIt.getCode().equals("INT") && localizationIt.getCode().equals("en")){
+                            hreflang = "x-default";
+                        }
+                        HeadLinkViewBean metaLink = new HeadLinkViewBean("alternate", hreflang, alternateUrl);
+                        alternateLinks.add(metaLink);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+        return alternateLinks;
     }
     
 }
--- apis/api-core/api-core-common-web-fo/src/main/java/org/hoteia/qalingo/core/web/mvc/controller/AbstractFrontofficeQalingoController.java
+++ apis/api-core/api-core-common-web-fo/src/main/java/org/hoteia/qalingo/core/web/mvc/controller/AbstractFrontofficeQalingoController.java
@@ -9,6 +9,8 @@
  */
 package org.hoteia.qalingo.core.web.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -16,14 +18,20 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.hoteia.qalingo.core.ModelConstants;
+import org.hoteia.qalingo.core.domain.Localization;
+import org.hoteia.qalingo.core.domain.MarketArea;
+import org.hoteia.qalingo.core.domain.MarketPlace;
+import org.hoteia.qalingo.core.domain.enumtype.FoUrls;
 import org.hoteia.qalingo.core.i18n.FoMessageKey;
 import org.hoteia.qalingo.core.i18n.enumtype.I18nKeyValueUniverse;
 import org.hoteia.qalingo.core.i18n.enumtype.ScopeCommonMessage;
 import org.hoteia.qalingo.core.i18n.enumtype.ScopeWebMessage;
+import org.hoteia.qalingo.core.pojo.RequestData;
 import org.hoteia.qalingo.core.security.helper.SecurityUtil;
 import org.hoteia.qalingo.core.security.util.SecurityRequestUtil;
 import org.hoteia.qalingo.core.service.CustomerService;
 import org.hoteia.qalingo.core.web.mvc.factory.FrontofficeViewBeanFactory;
+import org.hoteia.qalingo.core.web.mvc.viewbean.HeadLinkViewBean;
 import org.hoteia.qalingo.core.web.util.PropertiesUtil;
 import org.hoteia.qalingo.core.web.util.RequestUtil;
 import org.slf4j.Logger;
@@ -122,4 +130,43 @@ public abstract class AbstractFrontofficeQalingoController extends AbstractQalin
         return coreMessageSource.getSpecificMessage(I18nKeyValueUniverse.FO.getPropertyKey(), scope, key, params, locale);
     }
 
+    protected List<HeadLinkViewBean> buildConditionsOfUseLinkViewBeans(final RequestData requestData, FoUrls url) {
+        final MarketPlace marketPlace = requestData.getMarketPlace();
+        final MarketArea marketArea = requestData.getMarketArea();
+        
+        List<Long> allOpenedMarketAreaIds = marketService.findMarketAreaOpenedByMarketPlace(marketPlace);
+        List<MarketArea> allMarketArea = new ArrayList<MarketArea>();
+        for (Long marketareaId : allOpenedMarketAreaIds) {
+            MarketArea reloadedMarketArea = marketService.getMarketAreaById(marketareaId);
+            allMarketArea.add(reloadedMarketArea);
+        }
+        List<HeadLinkViewBean> alternateLinks = new ArrayList<HeadLinkViewBean>();
+        try {
+            for (MarketArea marketAreaIt : allMarketArea) {
+                for (Localization localizationIt : marketAreaIt.getLocalizations()) {
+                    if(!marketAreaIt.getCode().equals(marketArea.getCode())){
+    
+                        RequestData requestDataStore = new RequestData();
+                        requestDataStore.setMarketPlace(marketPlace);
+                        requestDataStore.setMarket(marketAreaIt.getMarket());
+                        requestDataStore.setMarketArea(marketAreaIt);
+                        requestDataStore.setMarketAreaLocalization(localizationIt);
+                        requestDataStore.setContextNameValue("FO_MCOMMERCE");
+                        
+                        String alternateUrl = urlService.buildAbsoluteUrl(requestDataStore, urlService.generateUrl(url, requestDataStore));
+                        String hreflang = localizationIt.getCode().replace("_", "-");
+                        if(marketAreaIt.getCode().equals("INT") && localizationIt.getCode().equals("en")){
+                            hreflang = "x-default";
+                        }
+                        HeadLinkViewBean metaLink = new HeadLinkViewBean("alternate", hreflang, alternateUrl);
+                        alternateLinks.add(metaLink);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+        return alternateLinks;
+    }
+    
 }
