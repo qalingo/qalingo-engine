e84ee7411f4155b495da3b39bd7e70b7e32e11f7
Denis <Denis@Hoteia-M4800>
2016-05-14 20:10:43 +0200

    fix

--- apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/service/BackofficeUrlService.java
+++ apis/api-core/api-core-common/src/main/java/org/hoteia/qalingo/core/service/BackofficeUrlService.java
@@ -104,81 +104,62 @@ public class BackofficeUrlService extends AbstractUrlService {
                     if (param instanceof CatalogCategoryMaster) {
                         CatalogCategoryMaster catalogCategoryMaster = (CatalogCategoryMaster) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_CATALOG_CATEGORY_CODE, handleParamValue(catalogCategoryMaster.getCode()));
-                        break;
                     } else if (param instanceof CatalogCategoryVirtual) {
                         CatalogCategoryVirtual catalogCategoryVirtual = (CatalogCategoryVirtual) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_CATALOG_CATEGORY_CODE, handleParamValue(catalogCategoryVirtual.getCode().toString()));
-                        break;
                     } else if (param instanceof ProductMarketing) {
                         ProductMarketing productMarketing = (ProductMarketing) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_PRODUCT_MARKETING_CODE, handleParamValue(productMarketing.getCode().toString()));
-                        break;
                     } else if (param instanceof ProductSku) {
                         ProductSku productSku = (ProductSku) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_PRODUCT_SKU_CODE, handleParamValue(productSku.getCode().toString()));
-                        break;
                     } else if (param instanceof Asset) {
                         Asset asset = (Asset) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_ASSET_ID, handleParamValue(asset.getId().toString()));
-                        break;
                     } else if (param instanceof AbstractRuleReferential) {
                         AbstractRuleReferential rule = (AbstractRuleReferential) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_RULE_CODE, handleParamValue(rule.getCode().toString()));
-                        break;
                     } else if (param instanceof Warehouse) {
                         Warehouse warehouse = (Warehouse) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_WAREHOUSE_CODE, handleParamValue(warehouse.getCode().toString()));
-                        break;
                     } else if (param instanceof DeliveryMethod) {
                         DeliveryMethod deliveryMethod = (DeliveryMethod) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_DELIVERY_METHOD_CODE, handleParamValue(deliveryMethod.getCode().toString()));
-                        break;
                     } else if (param instanceof Tax) {
                         Tax tax = (Tax) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_TAX_CODE, handleParamValue(tax.getCode().toString()));
-                        break;
                     } else if (param instanceof OrderPurchase) {
                         OrderPurchase order = (OrderPurchase) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_ORDER_NUM, handleParamValue(order.getOrderNum().toString()));
-                        break;
                     } else if (param instanceof Customer) {
                         Customer customer = (Customer) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_CUSTOMER_CODE, handleParamValue(customer.getCode().toString()));
-                        break;
                     } else if (param instanceof EngineSetting) {
                         EngineSetting engineSetting = (EngineSetting) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_ENGINE_SETTING_CODE, handleParamValue(engineSetting.getCode().toString()));
-                        break;
                     } else if (param instanceof EngineSettingValue) {
                         EngineSettingValue engineSettingValue = (EngineSettingValue) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_ENGINE_SETTING_CODE, handleParamValue(engineSettingValue.getEngineSetting().getCode().toString()));
                         getParams.put(RequestConstants.REQUEST_PARAMETER_ENGINE_SETTING_VALUE_CONTEXT, handleParamValue(engineSettingValue.getContext()));
-                        break;
                     } else if (param instanceof AbstractPaymentGateway) {
                         AbstractPaymentGateway paymentGateway = (AbstractPaymentGateway) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_PAYMENT_GATEWAY_CODE, handleParamValue(paymentGateway.getCode().toString()));
-                        break;
                     } else if (param instanceof User
                             && !urlWithoutWildcard.equals(BoUrls.PERSONAL_DETAILS.getUrlWithoutWildcard()) && !urlWithoutWildcard.equals(BoUrls.PERSONAL_EDIT.getUrlWithoutWildcard())) {
                         User user = (User) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_USER_CODE, handleParamValue(user.getLogin()));
-                        break;
                     } else if (param instanceof Retailer){ 
                     	Retailer retailer = (Retailer) param;
                     	getParams.put(RequestConstants.REQUEST_PARAMETER_RETAILER_CODE, handleParamValue(retailer.getCode()));
-                    	break;
                     } else if (param instanceof Store){ 
                         Store store = (Store) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_STORE_CODE, handleParamValue(store.getCode()));
-                        break;
                     } else if (param instanceof CmsContent){ 
                         CmsContent cmsContent = (CmsContent) param;
                         getParams.put(RequestConstants.REQUEST_PARAMETER_CMS_CONTENT_ID, cmsContent.getId().toString());
-                        break;
                     } else if (param instanceof Map) {
                         Map<String, String> paramMap = (Map<String, String>) param;
                         getParams.putAll(paramMap);
-                        break;
                     } else {
                         logger.info("Unknowned url parameter : [{}]", param);
                     }
